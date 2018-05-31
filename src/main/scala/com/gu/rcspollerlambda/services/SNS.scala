c4d764ef6d0f71353c65305da6ac91b446ded8d3
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2018-05-31 17:18:06 +0100

    Print size in bytes

--- src/main/scala/com/gu/rcspollerlambda/services/SNS.scala
+++ src/main/scala/com/gu/rcspollerlambda/services/SNS.scala
@@ -6,7 +6,7 @@ import io.circe.Json
 
 object SNS extends Config with Logging {
   def publish(message: Json): Either[String, Unit] = {
-    logger.info(s"Sending json to SNS stream...")
-    try { Right(AWS.snsClient.publish(new PublishRequest(AWS.topicArn, message.toString(), "update-rcs-rights"))) } catch { case e: Throwable => Left(s"Error while sending message to SNS: ${e.getMessage}") }
+    logger.info(s"Sending json to SNS stream (size ${message.noSpaces.getBytes.length})...")
+    try { Right(AWS.snsClient.publish(new PublishRequest(AWS.topicArn, message.noSpaces, "update-rcs-rights"))) } catch { case e: Throwable => Left(s"Error while sending message to SNS: ${e.getMessage}") }
   }
 }
