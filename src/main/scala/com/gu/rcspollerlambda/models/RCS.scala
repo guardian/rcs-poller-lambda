35e454b8796c0d232b1fc715580156d1fef55e4f
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2018-06-05 16:47:35 +0100

    Rename to SyndicationRights

--- src/main/scala/com/gu/rcspollerlambda/models/RCS.scala
+++ src/main/scala/com/gu/rcspollerlambda/models/RCS.scala
@@ -50,7 +50,7 @@ object RightsBatch {
         Supplier(supplierName, supplierId)
       }
 
-      RCSUpdate(tagSetId.get, id, RCSData(published, suppliers, prAgreement, rights))
+      RCSUpdate(tagSetId.get, id, SyndicationRights(published, suppliers, prAgreement, rights))
     }
 
     val lastPosition = rightsUpdates.lastOption.map(_.tagSetId)
@@ -88,15 +88,15 @@ object DateTimeFormatter {
   }
 }
 
-case class RCSUpdate(tagSetId: Long, id: String, data: RCSData)
+case class RCSUpdate(tagSetId: Long, id: String, data: SyndicationRights)
 object RCSUpdate {
   implicit val encoder: Encoder[RCSUpdate] = deriveEncoder[RCSUpdate]
 }
 
-case class RCSData(published: Option[DateTime], suppliers: Seq[Supplier], prAgreement: Option[Boolean], rights: Seq[Right])
-object RCSData {
+case class SyndicationRights(published: Option[DateTime], suppliers: Seq[Supplier], prAgreement: Option[Boolean], rights: Seq[Right])
+object SyndicationRights {
   import DateTimeFormatter._
-  implicit val encoder: Encoder[RCSData] = deriveEncoder[RCSData]
+  implicit val encoder: Encoder[SyndicationRights] = deriveEncoder[SyndicationRights]
 }
 
 case class Supplier(supplierName: Option[String], supplierId: Option[String])
