c67bf997631867f8b0cb96e6a8e497026a369ee0
Tony McCrae <tony.mccrae.freelance@guardian.co.uk>
2019-02-14 11:09:21 +0000

    Attempt to remap the JSON message fields into the format expected by Grid Kinesis.

--- src/main/scala/com/gu/rcspollerlambda/services/Kinesis.scala
+++ src/main/scala/com/gu/rcspollerlambda/services/Kinesis.scala
@@ -3,12 +3,12 @@ package com.gu.rcspollerlambda.services
 import java.util.UUID
 
 import com.amazonaws.services.kinesis.model.PutRecordRequest
-import com.amazonaws.services.kinesis.{ AmazonKinesis, AmazonKinesisClientBuilder }
+import com.amazonaws.services.kinesis.{AmazonKinesis, AmazonKinesisClientBuilder}
 import com.gu.rcspollerlambda.config.Config
-import com.gu.rcspollerlambda.models.{ KinesisPublishError, LambdaError }
+import com.gu.rcspollerlambda.models.{KinesisPublishError, LambdaError}
 import io.circe.generic.semiauto.deriveEncoder
 import io.circe.syntax._
-import io.circe.{ Encoder, Json, Printer }
+import io.circe.{Encoder, Json, Printer}
 
 object Kinesis extends Config with Logging {
 
@@ -18,13 +18,25 @@ object Kinesis extends Config with Logging {
     build()
 
   def publish(subject: String, message: Json): Either[LambdaError, Unit] = {
-    val updateMessage = UpdateMessage(subject = subject, rcsUpdate = message)
+
+    def updateMessageFromJSON = {
+      // message is a Json representation of a RCSUpdate
+      // case class RCSUpdate(tagSetId: Long, id: String, data: SyndicationRights)
+      //
+      // Grid is expecting an image id on field 'id' and a SyndicationRights on field syndicationRights.
+      // tagSetId is not consumed by the Grid
+
+      val imageId: Option[String] = (message \\ "id").headOption.flatMap(_.asString)
+      val syndicationRights: Option[Json] = (message \\ "data").headOption
+
+      UpdateMessage(subject = subject, id = imageId, syndicationRights = syndicationRights)
+    }
 
     val partitionKey = UUID.randomUUID().toString
 
     implicit val encoder: Encoder[UpdateMessage] = deriveEncoder[UpdateMessage]
     val printer = Printer.noSpaces.copy(dropNullValues = true)
-    val payload = printer.prettyByteBuffer(updateMessage.asJson)
+    val payload = printer.prettyByteBuffer(updateMessageFromJSON.asJson)
 
     try {
       Right {
@@ -42,4 +54,4 @@ object Kinesis extends Config with Logging {
 
 }
 
-case class UpdateMessage(subject: String, rcsUpdate: Json) // TODO not happy about the Json type can't bypass the top level toJson step
+case class UpdateMessage(subject: String, id: Option[String], syndicationRights: Option[Json]) // TODO not happy about the Json type can't bypass the top level toJson step
